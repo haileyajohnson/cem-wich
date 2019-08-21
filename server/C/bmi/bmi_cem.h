ba6ad3053ade72e79e4891f57263e3efae911e93
haileyajohnson <hailey.johnson@ufl.edu>
2019-08-21 15:21:57 -0400

    remove extra files

--- server/C/bmi/bmi_cem.h
+++ server/C/bmi/bmi_cem.h
@@ -8,9 +8,11 @@ extern "C" {
 #include "bmi.h"
 #include "bmi_EXPORTS.h"
 
-BMI_Model bmi_EXPORT *register_bmi_cem(BMI_Model *model);
+bmi_EXPORT BMI_Model *register_bmi_cem(BMI_Model *model);
 
-typedef struct { double time; } bmi_EXPORT CemModel;
+bmi_EXPORT int testFunc(int a, int b);
+
+bmi_EXPORT typedef struct { double time; } CemModel;
 
 #if defined(__cplusplus)
 }
