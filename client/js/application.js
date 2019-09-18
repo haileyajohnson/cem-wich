053ac1f9c209117d2639ce2e00f77ca6c2b8b9a2
Johnson <hailey.johnson@ufl.edu>
2019-09-18 17:11:57 -0400

    refactor to tab format

--- client/js/application.js
+++ client/js/application.js
@@ -2,225 +2,84 @@
 /**
  * Global variables
  */
+// config vars
 var configJSON;
+const CLIENT_ID = '762501139172-rjf0ia3vv9edu6gg0m46aoij519khuk7.apps.googleusercontent.com';
+
+// application interface objects
 var mapInterface;
 var modelInterface;
+
+
+// views
+var selectedTab;
+var gridTab;
+var wavTab;
+var controlTab;
+var runTab;
 var modal;
 
-const CLIENT_ID = '762501139172-rjf0ia3vv9edu6gg0m46aoij519khuk7.apps.googleusercontent.com';
-var selectedTab;
-var tabs;
-
-var $lon1;
-var $lat1;
-var $lon2;
-var $lat2;
-var $rotation;
-
-function Tab(id, elem, loadFunc) {
-    return {
-        id: id,
-        elem: elem,
-        loadFunc: loadFunc
-    };
-}
-
+// jQuery
+var $saveButton;
+var $loadButton;
 
 function loadApp() {
-    var onImmediateFailed = function () {
-        $('.g-sign-in').removeClass('hidden');
-        $('.output').text('(Log in to see the result.)');
-        $('.g-sign-in .button').click(function () {
-            ee.data.authenticateViaPopup(function () {
-                // If the login succeeds, hide the login button and run the analysis.
-                $('.g-sign-in').addClass('hidden');
-                initialize();
-            });
-        });
-    };
     // Attempt to authenticate using existing credentials.
-    ee.data.authenticate(CLIENT_ID, initialize, null, null, onImmediateFailed);
+    ee.data.authenticate(CLIENT_ID, initialize, null, null, googleSignIn);
 }
 
 function initialize() {
     configJSON = {};
 
+    // create applicaiton interfaces
     mapInterface = MapInterface();
     mapInterface.initMap();
     
     // create tabs
-    tabs = []
-    tabs.push(Tab(0, '#gridTab', gridTabView));
-    tabs.push(Tab(1, '#waveTab', waveTabView));
-    tabs.push(Tab(2, '#condsTab', condsTabView));
-    tabs.push(Tab(3, '#runTab', runTabView));
-    $.each(tabs, (i, t) => { $(t.elem).on('click', () => { onTabChange(t.id); })});
-    selectedTab = tabs[0];
-    $(selectedTab.elem).attr("selected", "selected");
-
-    // set up initial input screen
-    initInputs();
-
+    gridTab = GridTab();
+    gridTab.init();
+    waveTab = WaveTab();
+    waveTab.init();
+    controlTab = ControlsTab()
+    controlTab.init();
+    runTab = RunTab();
+    runTab.init();
+    
+    // create modal dialog window
     modal = ModalInterface();
     modal.init();
+
+    // init buttons
+    $saveButton = $(".save-button");
+    $loadButton = $(".load-button");
+
+    // open grid tab
+    onTabChange(gridTab);
 }
 
-function initInputs() {
-    // TODO: refactor
-    $rotation = $("input[name=rotation]");
-    $rotation.val(rad_to_deg(mapInterface.rotation));
-    $rotation.change(onRotationChange);
-    var $numRows = $("input[name=numRows]");
-    $numRows.val(mapInterface.numRows);
-    $numRows.change(onNumRowsChange);
-    var $numCols = $("input[name=numCols]");
-    $numCols.val(mapInterface.numCols);
-    $numCols.change(onNumColsChange);
-
-    
-    $lon1 = $("input[name=lon1]");
-    $lat1 = $("input[name=lat1]");
-    $lon2 = $("input[name=lon2]");
-    $lat2 = $("input[name=lat2]");
-
-    $(".coord").change(onCoordsChange);
+function googleSignIn() {
+    $('.g-sign-in').removeClass('hidden');
+    $('.output').text('(Log in to see the result.)');
+    $('.g-sign-in .button').click(function () {
+        ee.data.authenticateViaPopup(function () {
+            // If the login succeeds, hide the login button and run the analysis.
+            $('.g-sign-in').addClass('hidden');
+            initialize();
+        });
+    });
 }
-
 
 /**
- * Tab views
+ *  Event listeners 
  */
-
- function gridTabView() {
-    $(".grid-tab-view").show();
- }
-
- function waveTabView() {
-    $(".wave-tab-view").show();
- }
-
- function condsTabView() {
-    $(".conds-tab-view").show();
- }
-
- function runTabView() {
-    $(".run-tab-view").show();
- }
-
-/**
- *  Event listeners and
- *  event callbacks
- */
-
-function onTabChange(id) {
-    if (id == selectedTab.id) { return; }
-    // clear current tab
-    $(selectedTab.elem).removeAttr("selected");
-    $(".tab-view-container").children().hide();
-
-    //selecte new tab
-    selectedTab = tabs[id];
-    $(selectedTab.elem).attr("selected", "selected");
-    selectedTab.loadFunc();
-}
-
-function onGoClicked() {
-    disableInputs();
-    mapInterface.mapTransform();
-    disableClear();
-}
-
-function onDrawClicked() {
-    mapInterface.toggleDrawMode();
-}
-
-function onBoxChange() {
-    var coords = mapInterface.box.getCoordinates()[0];
-    $("input[name=lon1]").val(coords[0][0]);
-    $("input[name=lat1]").val(coords[0][1]);
-    $("input[name=lon2]").val(coords[2][0]);
-    $("input[name=lat2]").val(coords[2][1]);
-}
-
-function onRotationChange(rot) {    
-    $rotation.val(rad_to_deg(rot));
-    mapInterface.map.getView().setRotation(
-        deg_to_rad(tryParseFloat($rotation.val()), mapInterface.rotation));
-}
-
-
-
-function onCoordsChange() {
-    var coords = mapInterface.box ? mapInterface.box.getCoordinates()[0] : [["", ""], [], ["", ""]];
-    var lon1 = tryParseFloat($lon1.val(), coords[0][0]);
-    var lat1 = tryParseFloat($lat1.val(), coords[0][1]);
-    var lon2 = tryParseFloat($lon2.val(), coords[2][0]);
-    var lat2 = tryParseFloat($lat2.val(), coords[2][1]);
-
-    if ($(".coord").toArray().some((elem) => {
-        return !($(elem).val());
-    })) {
-        return;
+function onTabChange(newTab){
+    if (selectedTab) {
+        selectedTab.$tab.removeAttr("selected");
+        selectedTab.$elem.hide();
     }
-    mapInterface.updateBox(lon1, lat1, lon2, lat2);
-}
-
-function onNumRowsChange() {
-
-    mapInterface.setNumRows(tryParseInt($("input[name=numRows]").val()), mapInterface.numRows);
-    if (mapInterface.box) {
-        mapInterface.drawGrid();
-    }
-}
-
-function onNumColsChange() {
-    mapInterface.setNumCols(tryParseInt($("input[name=numCols]").val()), mapInterface.numCols);
-    if (mapInterface.box) {
-        mapInterface.drawGrid();
-    }
-}
-
-function onClear() {
-    mapInterface.clearMap();
-    enableInputs();
-    $(".coord").val("");
-}
-
-function disableInputs() {
-    $(".coord").attr("disabled", "disabled");
-    $(".draw-button").attr("disabled", "disabled");
-    $(".go-button").attr("disabled", "disabled");
-    //$(".edit-button").attr("disabled", "disabled").removeAttr("selected");
-}
-
-function enableInputs() {
-    $(".coord").removeAttr("disabled");
-    $(".draw-button").removeAttr("disabled");
-    $(".go-button").removeAttr("disabled");
-    //$(".edit-button").removeAttr("disabled");
-}
-
-function allowClear() {
-    $(".clear-button").removeAttr("disabled");
-}
-
-function disableClear() {
-    $(".clear-button").attr("disabled", "disabled");    
-}
-
-function toggleEdit() {
-    $(".edit-button").attr("selected") ? $(".edit-button").removeAttr("selected") : $(".edit-button").attr("selected", "selected");
-    mapInterface.toggleEditMode();
-}
-
-function onOkClicked() {
-    // save changes
-    modal.save(this.mapInterface);
-    modal.close();
-}
-
-function onCancelClicked() {
-    modal.close();
+    newTab.$tab.attr("selected", "selected");
+    newTab.$elem.show();
+    selectedTab = newTab;
 }
 
 function onSave() {
@@ -233,7 +92,7 @@ function onLoad() {
 }
 
 /**
- * Save/load function
+ * Save/load functions
  */
 
 function updateJSON() {
@@ -274,22 +133,15 @@ function importJSON(newContent) {
 
     // load grid inputs
     var gridConfig = configJSON.gridConfig;
-    onRotationChange(gridConfig.rotation);    
-    var $numRows = $("input[name=numRows]");
-    $numRows.val(gridConfig.numRows); 
-    onNumColsChange()  
-    var $numCols = $("input[name=numCows]");
-    $numCols.val(gridConfig.numCols);
-    onNumColsChange();
+    mapInterface.setNumRows(gridConfig.numRows);
+    mapInterface.setNumCols(gridConfig.numCols);
+    mapInterface.map.getView().setRotation(gridConfig.rotation);  
     mapInterface.cemGrid = gridConfig.grid;
     if (gridConfig.points) {
         var points = gridConfig.points;
-        $("input[name=lon1]").val(points[0][0]);
-        $("input[name=lat1]").val(points[0][1]);
-        $("input[name=lon2]").val(points[1][0]);
-        $("input[name=lat2]").val(points[1][1]);
-        onCoordsChange();
+        mapInterface.updateBox(points[0][0], points[0][1], points[1][0], points[1][1]);
     }
+    gridTab.setAllValues();
 
     // load wave inpts
 
@@ -306,7 +158,8 @@ function saveConfig() {
 
  function loadConfig() {
     // getting a hold of the file reference
-    var file = this.event.target.files[0]; 
+    var file = this.event.target.files[0];
+    if (!file) { return; }
     
     // setting up the reader
     var reader = new FileReader();
@@ -316,11 +169,213 @@ function saveConfig() {
         var content = readerEvent.target.result;
         importJSON(content)
     };
+    this.event.target.value = '';
  }
 
- /**
-  * Modal window
-  */
+/**
+ * Grid tab view
+ */
+function GridTab() {
+    return {
+        $elem: $(".grid-tab"),
+        $tab: $("#grid-tab"),
+        
+        $rotation: $("input[name=rotation]"),
+        
+        $numRows: $("input[name=numRows]"),       
+        $numCols: $("input[name=numCols]"),
+
+        $lon1: $("input[name=lon1]"),
+        $lat1: $("input[name=lat1]"),
+        $lon2: $("input[name=lon2]"),
+        $lat2: $("input[name=lat2]"),
+        $coords: $(".coord"),
+
+        $drawButton: $(".draw-button"),
+        $submitButton: $(".submit-button"),
+        $clearButton: $(".clear-button"),
+        $editButton: $(".edit-button"),
+
+        init: function() {
+            // add input listeners
+            this.$tab.click(() => { onTabChange(this); });
+            this.$rotation.change(() => { this.onRotationChange(); });
+            this.$numRows.change(() => { this.onNumRowsChange(); });
+            this.$numCols.change(() => { this.onNumColsChange(); });
+            this.$coords.change(() => { this.onCoordsChange(); });
+
+            // add button listeners
+            this.$drawButton.click(() => { this.onDrawClicked(); });
+            this.$submitButton.click(() => { this.onSubmitClicked(); });
+            this.$clearButton.click(() => { this.onClearClicked(); });
+            this.$editButton.click(() => { this.toggleEdit(); });
+
+            // disable buttons
+            this.$submitButton.disable();
+            this.$clearButton.disable();
+            this.$editButton.disable();
+
+            // initialize
+            this.setAllValues();
+        },
+        
+        /**
+         * click listeners
+         */
+        onSubmitClicked: function() {
+            this.$coords.disable();
+
+            this.$drawButton.disable();
+            $loadButton.disable();
+
+            mapInterface.mapTransform();
+            
+            this.$editButton.enable();
+        },
+
+        onClearClicked: function() {
+            mapInterface.clearMap();
+
+            this.$rotation.enable();
+            this.setRotation();
+            this.$coords.val("");
+            this.$coords.enable();
+            
+            this.$drawButton.enable();
+            this.$submitButton.disable();
+            this.$editButton.disable();
+            this.$clearButton.disable();
+            
+            $loadButton.enable();
+        },
+
+        onDrawClicked: function() {
+            mapInterface.toggleDrawMode();
+        },
+
+        toggleEdit: function() {
+            this.$editButton.attr("selected") ? this.$editButton.removeAttr("selected") : this.$editButton.attr("selected", "selected");
+            mapInterface.toggleEditMode();
+        },
+
+        /**
+         * input listeners 
+         */
+        onCoordsChange: function() {
+            var coords = mapInterface.box ? mapInterface.box.getCoordinates()[0] : [["", ""], [], ["", ""]];
+            var lon1 = tryParseFloat(this.$lon1.val(), coords[0][0]);
+            var lat1 = tryParseFloat(this.$lat1.val(), coords[0][1]);
+            var lon2 = tryParseFloat(this.$lon2.val(), coords[2][0]);
+            var lat2 = tryParseFloat(this.$lat2.val(), coords[2][1]);
+
+            // if one or more input is empty
+            if ($(".coord").toArray().some((e) => { return !($(e).val());})) {
+                return;
+            }
+            mapInterface.updateBox(lon1, lat1, lon2, lat2);
+        },
+
+        onNumRowsChange: function() {
+            mapInterface.setNumRows(tryParseInt(this.$numRows.val()), mapInterface.numRows);
+            if (mapInterface.box) {
+                mapInterface.drawGrid();
+            }
+        },
+
+        onNumColsChange: function() {
+            mapInterface.setNumCols(tryParseInt(this.$numCols.val()), mapInterface.numCols);
+            if (mapInterface.box) {
+                mapInterface.drawGrid();
+            }
+        },
+
+        onRotationChange: function() {
+            mapInterface.map.getView().setRotation(
+                deg_to_rad(tryParseFloat(this.$rotation.val()), mapInterface.rotation));
+        },
+
+        /**
+         * setters
+         */        
+        setAllValues: function() {
+            this.setRotation();
+            this.$numRows.val(mapInterface.numRows);
+            this.$numCols.val(mapInterface.numCols);
+            this.setCoords();
+        },        
+
+        setRotation: function() {    
+            this.$rotation.val(rad_to_deg(mapInterface.rotation));
+        },        
+
+        setCoords: function() {
+            if (!mapInterface.box) { return; }
+            var coords = mapInterface.box.getCoordinates()[0];
+            $("input[name=lon1]").val(coords[0][0]);
+            $("input[name=lat1]").val(coords[0][1]);
+            $("input[name=lon2]").val(coords[2][0]);
+            $("input[name=lat2]").val(coords[2][1]);
+        },
+
+        /**
+         * map listeners
+         */
+
+        onBoxDrawn: function() {
+            this.setRotation();
+            this.setCoords();
+            this.$rotation.disable();
+            this.$clearButton.enable();
+            this.$submitButton.enable();
+        }
+    }
+}
+
+/**
+ * Wave tab view
+ */
+function WaveTab() {
+    return {
+        $elem: $(".wave-tab"),
+        $tab: $("#wave-tab"),
+
+        init: function() {
+            this.$tab.click(() => { onTabChange(this); });
+        }
+    }
+}
+
+/**
+* Controls tab view
+*/
+function ControlsTab() {
+    return {
+        $elem: $(".control-tab"),
+        $tab: $("#control-tab"),
+
+        init: function() {
+            this.$tab.click(() => { onTabChange(this); });
+        }
+    }
+}
+
+/**
+ * Run tab view
+ */
+function RunTab() {
+    return {
+        $elem: $(".run-tab"),
+        $tab: $("#run-tab"),
+
+        init: function() {
+            this.$tab.click(() => { onTabChange(this); });
+        }
+    }
+}
+
+/**
+ * Modal window
+ */
  function ModalInterface() {
     return {
         $modal: $(".modal"),
@@ -333,6 +388,7 @@ function saveConfig() {
         orientation: 0,
 
         init: function() {
+            // set up listeners
             this.$fillInput.change(() => {
                 this.setFill();
                 this.display();
@@ -343,6 +399,8 @@ function saveConfig() {
                     this.display();
                 });
             });
+            this.$modal.find(".modal-ok-button").click(() => { this.onOkClicked(); } );
+            this.$modal.find(".modal-cancel-button").click(() => { this.onCancelClicked() });
         },
 
         open: function(feature) {
@@ -405,7 +463,7 @@ function saveConfig() {
             }
         },
 
-        save: function(mapInterface) {
+        save: function() {
             var i = this.feature.get('id');
             mapInterface.cemGrid[i] = this.percentFull/100;
             mapInterface.updateFeature(this.feature, this.percentFull/100, this.orientation);
@@ -414,6 +472,19 @@ function saveConfig() {
         close: function() {
             this.feature = null;
             this.$modal.hide();
+        },
+
+        /**
+         * Listeners
+         */        
+        onOkClicked: function() {
+            // save changes
+            this.save();
+            this.close();
+        },
+
+        onCancelClicked: function() {
+            this.close();
         }
     };
 }
