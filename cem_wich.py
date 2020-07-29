df89b037c72bbb38a4657acaadefa17206e9cfaf
haileyajohnson <hailey.johnson@ufl.edu>
2020-07-29 00:52:42 -0400

    run on aws

--- cem_wich.py
+++ cem_wich.py
@@ -345,4 +345,4 @@ if __name__ == "__main__":
     service_account = 'cem-ee-utility@cem-ee-utility.iam.gserviceaccount.com'    
     credentials = ee.ServiceAccountCredentials(service_account, '../private_key.json')
     ee.Initialize(credentials)
-    app.run()
+    app.run(host='0.0.0.0')
